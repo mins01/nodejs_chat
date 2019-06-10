4cc84a2e40fae50a4c3a343ff3912158a15797ab
공대여자 <mins01.lycos.co.kr@gmail.com>
2019-06-10 18:27:56 +0900

    나오는것 확인

--- client/tetrisOnline.js
+++ client/tetrisOnline.js
@@ -1,12 +1,9 @@
 "use strict"
 
 var tetrisOnline = {
+	"ttrg":null,
 	"ttrgs":{},
 	"uid":"",
-	"jsonHandler":function(json){
-		console.log(json);
-		this.createTetris(json.uid);
-	},
 	"createTetris":function(uid){
 		if(this.ttrgs[uid]){return this.ttrgs[uid];}
 		var gameBox = document.querySelector("#gameboxHtml").content.cloneNode(true).querySelector('.gameBox');
@@ -14,6 +11,7 @@ var tetrisOnline = {
 		var gameBoxes = document.querySelector(".gameBoxes");
 		gameBoxes.append(gameBox);
 		var ttrg = tetrisBoxGame();
+		var tetrisGame = ttrg;
 		ttrg.title="p1";
 		ttrg.init(gameBox.querySelector('.ttrbg-layout'));
 		ttrg.create(10,20);
@@ -24,6 +22,39 @@ var tetrisOnline = {
 			ttrg.onresize();
 		})
 		this.ttrgs[uid]=ttrg;
+		if(controller.v.user.uid == uid){
+			this.ttrg = ttrg;
+			ttrg.ttr.cbOnDraw = function(map,w,h,mapNext,info){
+				tetrisGame.draw(map,w,h,mapNext,info);
+				controller.send(new MsgObj({app:"tetris","fun":"draw",val:[map,w,h,mapNext,info]}));
+			}
+			// this.start();
+		}else{
+			ttrg.startClient();
+		}
+		
+		
 		return this.ttrgs[uid];
+	},
+	"start":function(){
+		var ttrg = this.createTetris(controller.v.user.uid)
+		ttrg.start();
+	},
+	"jsonHandler":function(json){
+		// console.log(json);
+		var uid = json.uid;
+		var ttrg = this.createTetris(uid);
+		switch (json.fun) {
+			case "xxx":
+				
+				break;
+			default:
+			if(ttrg[json.fun]!=null){
+				ttrg[json.fun].apply(ttrg,json.val);
+			}
+			break;
+				
+		}
 	}
+	
 }
