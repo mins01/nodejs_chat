cbe6d18d22f098d7498c172a4be01faccea69b4c
공대여자 <mins01.lycos.co.kr@gmail.com>
2019-06-10 17:19:37 +0900

    생성까지 완료

--- client/tetrisOnline.js
+++ client/tetrisOnline.js
@@ -1,8 +1,29 @@
 "use strict"
 
 var tetrisOnline = {
+	"ttrgs":{},
 	"uid":"",
 	"jsonHandler":function(json){
 		console.log(json);
+		this.createTetris(json.uid);
+	},
+	"createTetris":function(uid){
+		if(this.ttrgs[uid]){return this.ttrgs[uid];}
+		var gameBox = document.querySelector("#gameboxHtml").content.cloneNode(true).querySelector('.gameBox');
+		$(gameBox).attr("uid",uid);
+		var gameBoxes = document.querySelector(".gameBoxes");
+		gameBoxes.append(gameBox);
+		var ttrg = tetrisBoxGame();
+		ttrg.title="p1";
+		ttrg.init(gameBox.querySelector('.ttrbg-layout'));
+		ttrg.create(10,20);
+		$(document).on('keydown',function(event){
+			ttrg.onkeyDown(event)
+		})
+		$(window).on('resize',function(event){
+			ttrg.onresize();
+		})
+		this.ttrgs[uid]=ttrg;
+		return this.ttrgs[uid];
 	}
 }
